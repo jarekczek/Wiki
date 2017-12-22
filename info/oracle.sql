b95cbab19a9bb490b48c06c5babbccafcd472453
Jarek Czekalski <jaroslaw.czekalski@bonsoft.pl>
2017-12-22 20:34:12 +0100

    ora

--- info/oracle.sql
+++ info/oracle.sql
@@ -32,3 +32,15 @@ select to_char(sql.LAST_ACTIVE_TIME, 'yy-mm-dd hh24:mi:ss')
 order by sql.LAST_ACTIVE_TIME desc
 ;
 
+-- aktywne sesje i sql query
+select se.wait_class, se.state, se.TIME_REMAINING_MICRO
+, se.last_call_et /* liczba sekund trwania sesji (dla stanu active) */
+, sql.elapsed_time / 1000 / sql.EXECUTIONS
+, sql.LAST_ACTIVE_TIME, se.SQL_EXEC_START
+, sql.LAST_ACTIVE_TIME - se.SQL_EXEC_START
+, sql.sql_text, se.* from v$session se
+--join v$sql sql on se.SQL_HASH_VALUE = sql.HASH_VALUE
+join v$sql sql on se.SQL_id = sql.sql_id
+where se.program = 'sqlplus.exe'
+order by se.LOGON_TIME desc
+;
