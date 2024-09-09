31fc1959dbc1a6d29510d81b7506702cde78deb8
jarekczek <jarekczek@poczta.onet.pl>
2024-09-09 13:56:25 +0200

    postgres

--- info/postgres.sql
+++ info/postgres.sql
@@ -11,6 +11,11 @@ ALTER USER user_name WITH PASSWORD 'new_password';
 select * from pg_catalog.pg_namespace
 
 --grants
-SELECT * FROM information_schema.role_table_grants 
+SELECT * FROM information_schema.role_table_grants order by table_name, grantee, privilege_type;
 
 set search_path to xxx, yyy;
+
+select * from pg_catalog.pg_tables
+order by tablename;
+
+select * from pg_catalog.pg_default_acl order by defaclobjtype;
